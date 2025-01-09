a2e8205c4fc6c60b6eb71cf3c3b5b7ec40fb9d22
Kevon Kibochi <devokrann@gmail.com>
2025-01-09 13:18:14 +0300

    todo: remove unused pages

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import LayoutBody from '@/components/layout/body';
 import NavbarMain from '@/components/layout/navbars/main';
 import FooterMain from '@/components/layout/footers/main';
-
+import HeaderMain from '@/components/layout/headers/main';
+import HeroRoute from '@/components/layout/hero/route';
 import AffixTop from '@/components/common/affixi/top';
 import AffixNavbar from '@/components/common/affixi/navbar';
+import AffixWhatsApp from '@/components/common/affixi/whatsapp';
+// import AffixOffline from '@/components/common/affixi/offline';
+// import AffixCookies from '@/components/common/affixi/cookies';
 
 export default function LayoutMarketing({
   children, // will be a page or nested layout
@@ -13,11 +17,19 @@ export default function LayoutMarketing({
   children: React.ReactNode;
 }) {
   return (
-    <LayoutBody nav={<NavbarMain />} footer={<FooterMain />}>
+    <LayoutBody
+      header={<HeaderMain />}
+      nav={<NavbarMain />}
+      footer={<FooterMain />}
+      hero={<HeroRoute />}
+    >
       <main>{children}</main>
 
       <AffixTop />
       <AffixNavbar />
+      <AffixWhatsApp />
+      {/* <AffixOffline/> */}
+      {/* <AffixCookies /> */}
     </LayoutBody>
   );
 }
