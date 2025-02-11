b241039ef0fec1f41f1c4f1012468d6c2baaa7b0
Kevon Kibochi <devokrann@gmail.com>
2025-02-11 11:11:12 +0300

    todo: minor updates (#7)

--- src/components/layout/hero/home.tsx
+++ src/components/layout/hero/home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LayoutSection from '../section';
 import classes from './home.module.scss';
 import { SECTION_SPACING } from '@/data/constants';
-import { Stack, Text } from '@mantine/core';
+import { Divider, Stack, Text, Title } from '@mantine/core';
 import appData from '@/data/app';
 import ImageDefault from '@/components/common/images/default';
 import { images } from '@/assets/images';
@@ -13,7 +13,7 @@ export default function Home() {
       id="layout-hero"
       padded={SECTION_SPACING * 1.5}
       className={classes.hero}
-      containerized={'md'}
+      containerized={'sm'}
     >
       <Stack align="center" gap={SECTION_SPACING / 1.5}>
         <ImageDefault
@@ -23,28 +23,40 @@ export default function Home() {
           width={{ base: 205, sm: 310, md: 410 }}
         />
 
-        <Text
-          inherit
-          fz={{ base: 'xs', xs: 'sm', sm: 'md', md: 'lg' }}
-          ta={'center'}
-        >
+        <Text inherit fz={{ base: 'xs', xs: 'sm' }} ta={'center'}>
           Following the success of the 2024 AI Conference Nairobi, which
           attracted over 900 attendees, Drone Space is excited to announce that
           it will host the 2nd AI Conference Nairobi
           {/* on May 6th and 7th, 2025. */}
         </Text>
 
-        <Stack gap={'xs'} align="center" ta={'center'} fw={'bold'} lh={1.2}>
-          <Text inherit c={'sec.6'} fz={{ base: 'xl', md: 24 }}>
+        <Stack gap={'xs'} align="center" ta={'center'} lh={1.2}>
+          <Text inherit c={'sec.6'} fz={{ base: 'xl', md: 24 }} fw={'bold'}>
             Explore the Fusion of AI, Drones, and Data Analytics
           </Text>
-          <Text inherit fz={{ base: 'lg', xs: 24, md: 32 }}>
+          <Text inherit fz={{ base: 'lg', xs: 24, md: 32 }} fw={'bold'}>
             {appData.locations.main.location}
           </Text>
           {/* <Text inherit fz={{ base: 'lg', xs: 24, md: 32 }}>
             Tue 6th - Wed 7th May, 2025
           </Text> */}
-          <Text inherit fw={'normal'} fz={{ base: 'sm', xs: 'md', md: 'lg' }}>
+
+          <Stack>
+            <Title order={2} fz={'xl'} c={'sec'} ta={'center'}>
+              Upcoming Event: 2nd AI Conference Nairobi 2025
+            </Title>
+
+            <Text inherit fz={{ base: 'xs', xs: 'sm', sm: 'md' }} ta={'center'}>
+              We are excited to announce that we will be hosting the 2nd AI
+              Conference Nairobi 2025 at JW Marriott, Westlands. While the
+              official date is yet to be confirmed, preparations are underway
+              for an insightful and engaging event.
+            </Text>
+          </Stack>
+
+          <Divider color="white" w={'100%'} my={'xs'} />
+
+          <Text inherit fw={'normal'} fz={{ base: 'sm' }}>
             {appData.phones.main} | {appData.emails.info}
           </Text>
         </Stack>
