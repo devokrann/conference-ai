17fced194766c5c822e836cc1c354b641ea33688
Kevon Kibochi <devokrann@gmail.com>
2025-02-13 13:07:06 +0300

    refactor: enhance image modal with close button and improved styling; update gallery layout for better responsiveness

--- src/components/partial/gallery.tsx
+++ src/components/partial/gallery.tsx
@@ -1,22 +1,34 @@
 'use client';
 
 import { Grid, GridCol, Group, Pagination } from '@mantine/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ModalImage from '../common/modals/image';
 import { usePaginate } from '@/hooks/paginate';
+import { useRouter } from 'next/navigation';
 
 export default function Gallery({ props }: { props: { list: any[] } }) {
+  const router = useRouter();
+
   const divisor = 12;
   const { items, totalPages, activePage, setActivePage } = usePaginate(
     props.list,
     divisor
   );
 
+  useEffect(() => {
+    if (activePage > 1) {
+      router.push('#gallery');
+    }
+  }, [activePage]);
+
   return (
     <>
       <Grid justify="center" gutter={'xs'}>
         {items.map((item) => (
-          <GridCol key={item.image} span={{ base: 12, xs: 4, md: 3 }}>
+          <GridCol
+            key={item.image}
+            span={{ base: 12, xs: 6, sm: 4, md: 3, xl: 2 }}
+          >
             <ModalImage image={item.image} />
           </GridCol>
         ))}
